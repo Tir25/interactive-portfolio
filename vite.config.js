87ee59147058e47b78595cdf9bb8b9245cea118c
Tir25 <tirthraval27@gmail.com>
2025-04-24 01:14:34 +0530

    Update configuration for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { splitVendorChunkPlugin } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  base: '/interactive-portfolio/',
+  base: '/',
   plugins: [
     react(),
     splitVendorChunkPlugin(),
